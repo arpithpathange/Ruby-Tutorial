8574ab6b73f833de43e92f1afbafbb76447e323a
arpithpathange <arpith.rules@gmail.com>
2014-03-22 12:55:40 +0530

    learning

---
+++ user_input.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+
+print "Enter the first number ";
+vals1 = gets;
+print  "Enter the second number ";
+vals2 = gets;
+puts " The Sumation for the numbers ",(vals2.to_i+vals1.to_i) # need to convert it to integer before adding
+
+
